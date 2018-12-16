0ee5de17ee12f598c19f71074c7faab98bd58704
Jeff-Kung <42373551+Jeff-Kung@users.noreply.github.com>
2018-12-16 22:32:36 +0800

    Update index.php

--- index.php
+++ index.php
@@ -26,7 +26,7 @@
 </head>
 <body>
   <div align="center">
-    <h1>I finally made it - V9</h1>
+    <h1>I finally made it - V10</h1>
     <h2>Github + ALB + CodeDeploy + Jenkins</h2> 
     <h2>AWS BJS CodeDeploy Demo</h2>
   </div>
