c22da4995f36eb81744ea40842616968663e8741
Jeff-Kung <42373551+Jeff-Kung@users.noreply.github.com>
2019-01-28 11:12:40 +0800

    Update index.php

--- index.php
+++ index.php
@@ -27,8 +27,8 @@
 <body>
   <div align="center">
     <h1>Demo for Cathay Financial Holdings</h1>
-    <h2>Github + CodePipeline + CodeBuild +CodeDeploy with Blue/Green</h2> 
-    <h2>AWS Codepipeline Demo</h2>
+    <h2>This is how Load Balancer workds</h2> 
+    <h2>AWS Load Balancer Demo</h2>
   </div>
   </br>
   <div align="center">
@@ -37,7 +37,7 @@
     $zone = @file_get_contents("http://169.254.169.254/latest/meta-data/placement/availability-zone");
     $private_id = @file_get_contents("http://instance-data/latest/meta-data/local-ipv4");
   ?>
-    <img src="https://s3.cn-north-1.amazonaws.com.cn/bjsdemo/CodeDeploy-Application-Code/awschina.png" width="300"/>
+    <img src="https://az796311.vo.msecnd.net/userupload/2ab6e6628384412ea3b2c39ea6a67f0f.jpg" width="300"/>
     </br>    
     <h2>EC2 Instance ID: <?php echo $instance_id; ?></h2>
     <h2>EC2 Zone: <?php echo $zone; ?></h2>
